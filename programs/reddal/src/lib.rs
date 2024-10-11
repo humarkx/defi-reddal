1efd2b774db0c625480e7c047eed4413a1f3595c
humarkx <all@hugomarques.pt>
2024-10-11 13:15:24 +0100

    feat: normal escrow

--- programs/reddal/src/lib.rs
+++ programs/reddal/src/lib.rs
@@ -1,5 +1,7 @@
-// Import statements
 use anchor_lang::prelude::*;
+mod contexts;
+use contexts::*;
+mod states;
 
 // Rust Macro
 declare_id!("BPuZx3QmrJWSkWKQ1fo1rBeuYkP56Nyt5VuYNEPdkTiK");
@@ -7,14 +9,23 @@ declare_id!("BPuZx3QmrJWSkWKQ1fo1rBeuYkP56Nyt5VuYNEPdkTiK");
 #[program]
 pub mod reddal {
     use super::*;
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        initializer_amount: u64,
+        taker_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts
+            .initialize_escrow(seed, &ctx.bumps, initializer_amount, taker_amount)?;
+        ctx.accounts.deposit(initializer_amount)
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn exchange(ctx: Context<Exchange>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
     }
 }
-
-
-// Type declarations
-#[derive(Accounts)]
-pub struct Initialize {}
